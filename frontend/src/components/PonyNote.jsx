13440b680ad45f1f367bcd0fdf03b12ef1ba21ac
Olga Pinheiro <olgapinheirogarcia@gmail.com>
2018-07-12 22:49:17 -0300

    Added UI elements to add and edit notes

--- frontend/src/components/PonyNote.jsx
+++ frontend/src/components/PonyNote.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
-import {notes} from "../actions";
+import { connect } from 'react-redux';
+import { notes } from "../actions";
 
 class PonyNote extends Component {
+    state = {
+        text: "",
+        updateNoteId: null,
+    }
+
+    resetForm = () => {
+        this.setState({text: "", updateNoteId: null});
+    }
+
+    selectForEdit = (id) => {
+        let note = this.props.notes[id];
+        this.setState({text: note.text, updateNoteId: id});
+    }
+
+    submitNote = (e) => {
+        e.preventDefault();
+        if (this.state.updateNoteId === null){
+        this.props.addNote(this.state.text);
+        } else {
+            this.props.updateNote(this.state.updateNoteId, this.state.text);
+        }
+        this.resetForm();
+    }
+
     render() {
         return (
             <div>
@@ -13,18 +37,26 @@ class PonyNote extends Component {
                 <h3>Notes</h3>
                 <table>
                     <tbody>
-                        {this.props.notes.map(note => (
-                            <tr>
+                        {this.props.notes.map((note, id) => (
+                            <tr key={`note_${id}`}>
                                 <td>{note.text}</td>
-                                <td><button>edit</button></td>
-                                <td><button>delete</button></td>
+                                <td><button onClick={() => this.selectForEdit(id)}>edit</button></td>
+                                <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
                 <p>
-                    <Link to="/contact">Click Here</Link> to contact us!
-    </p>
+                    <h3>Add new note</h3>
+                    <form onSubmit={this.submitNote}>
+                        <input value={this.state.text}
+                            placeholder="Enter note here..."
+                            onChange={(e) => this.setState({ text: e.target.value })}
+                            required />
+                        <input type="submit" value="Save Note" />
+                        <button onClick={this.resetForm}>Reset</button>
+                    </form>
+                </p>
             </div>
         )
     }
